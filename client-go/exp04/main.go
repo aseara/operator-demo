4e6a3577085d85076a0a53831f700817f34fbd18
aseara <aseara@163.com>
2022-06-11 12:03:44 +0800

    clientset demo

--- client-go/exp04/main.go
+++ client-go/exp04/main.go
@@ -4,6 +4,9 @@ import (
 	"context"
 
 	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
@@ -32,6 +35,20 @@ func main() {
 		println("service get error", err)
 	} else {
 		println("service name: ", svc.Name)
+	}
+
+	config, err = clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	if err != nil {
+		panic(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	servicesClient := clientset.CoreV1().Services("kube-system")
+
+	svc2, err := servicesClient.Get(context.TODO(), "kube-dns", metav1.GetOptions{})
+	if err != nil {
+		println("service get error", err)
+	} else {
+		println("service name: ", svc2.Name)
 	}
 
 }
